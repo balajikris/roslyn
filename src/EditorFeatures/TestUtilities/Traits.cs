5c51afed3a646c59f3b9c7c152774644daa72089
Balaji Krishnan <balajik@microsoft.com>
2015-12-16 12:48:40 -0800

    initial tests for C# usevar.
    shelving, edit this commit later.

--- src/EditorFeatures/TestUtilities/Traits.cs
+++ src/EditorFeatures/TestUtilities/Traits.cs
@@ -71,6 +71,7 @@ namespace Roslyn.Test.Utilities
             public const string CodeActionsSpellcheck = "CodeActions.Spellcheck";
             public const string CodeActionsSuppression = "CodeActions.Suppression";
             public const string CodeActionsUseAutoProperty = "CodeActions.UseAutoProperty";
+            public const string CodeActionsUseImplicitTyping = "CodeActions.UseImplicitTyping";
             public const string CodeGeneration = "CodeGeneration";
             public const string CodeGenerationSortDeclarations = "CodeGeneration.SortDeclarations";
             public const string CodeModel = "CodeModel";
